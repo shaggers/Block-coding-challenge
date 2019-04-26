699568c8bc72ae36ffe254a58fa1893be5a50f34
shaggers <camtl66@gmail.com>
2019-04-26 06:35:01 -0500

    static route complete

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -6,10 +6,11 @@ describe("routes : static", () => {
 
   describe("GET /", () => {
 
-    it("should return status code 200", (done) => {
+    it("should return status code and say welcome in the response", (done) => {
 
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome");
         done();
       });
     });
